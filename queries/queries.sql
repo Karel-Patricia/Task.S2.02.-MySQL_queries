aa6c97509eb5bca44e7e27dc947e5bbd6de87f14
Patricia Paredes <patriciakarel@hotmail.com>
2026-02-06 20:50:06 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -322,7 +322,20 @@ WHERE precio = (
 );
 
 -- 40. Retorna tots els productes de la base de dades que tenen un preu major o igual al producte més car del fabricant Lenovo.
-
+SELECT 
+	codigo,
+    nombre,
+    precio
+FROM producto
+WHERE precio >= (
+	SELECT MAX(precio)
+    FROM producto
+    WHERE codigo_fabricante = (
+		SELECT codigo
+        FROM fabricante
+        WHERE nombre = 'Lenovo'
+	)
+);
 
 -- 41. Llista tots els productes del fabricant Asus que tenen un preu superior al preu mitjà de tots els seus productes.
 
